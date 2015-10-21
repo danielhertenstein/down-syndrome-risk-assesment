b90c7c7af347d3bb272f7b12c00da0941dcc58fc
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2015-10-21 12:55:02 -0400

    Adds new version that uses data binding.
    Hopefully, this one will be the real version. I'm using data binding in
    an attempt to explore the whole MVVM concept and keeping the UI separate
    from the model.

---
+++ DataBinding-Attempt/RiskAssessmentModel.cs
@@ -0,0 +1,48 @@
+﻿namespace DataBinding_Attempt
+{
+    using System;
+    using System.ComponentModel;
+
+    class RiskAssessmentModel : INotifyPropertyChanged
+    {
+        // a priori Risk
+        private double _aprioriRisk;
+        public double aprioriRisk
+        {
+            get { return _aprioriRisk; }
+            set
+            {
+                _aprioriRisk = value;
+                OnPropertyChanged("aprioriRisk");
+                OnPropertyChanged("adjustedRisk");
+            }
+        }
+
+        // Echogenic Focus Ratio
+
+        // Likelihood Ratio
+
+        // Adjusted Risk Factor
+        public double adjustedRisk
+        {
+            get { return aprioriRisk; }
+        }
+
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
