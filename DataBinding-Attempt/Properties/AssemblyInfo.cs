ca4b053e07bf89d15f12dd2c7c6028a255c3ebe7
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2015-10-27 14:04:17 -0400

    Updates assembly info for v.0.1.

--- DataBinding-Attempt/Properties/AssemblyInfo.cs
+++ DataBinding-Attempt/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataBinding-Attempt")]
+[assembly: AssemblyTitle("Down Syndrome Risk Assessment Model")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataBinding-Attempt")]
+[assembly: AssemblyCompany("Daniel Hertenstein")]
+[assembly: AssemblyProduct("Down Syndrome Risk Assessment Model")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
